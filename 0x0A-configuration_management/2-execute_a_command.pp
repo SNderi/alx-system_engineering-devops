57ed100aeaade92cdd6b9bc6e4976bf797d53f8a
Sharon Nderi <nderisharonw@gmail.com>
2022-09-16 17:09:04 +0000

    Added an optional argument to pkill

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
 # Creates a manifest that kills a process named killmenow.
 
 exec { 'pkill':
-    command  => 'pkill killmenow',
+    command  => 'pkill -f killmenow',
     path     => '/user/bin',
     provider => 'shell',
 }
