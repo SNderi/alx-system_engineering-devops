a920291b3ea1ecf0bac470c2b8e666cbd0e4a75b
SNderi <nderisharonw@gmail.com>
2022-09-18 11:17:51 +0300

    Add full colon

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,12 @@
 # Sets up client SSH configuration file to connect to a server without a password
 
 include stdlib
-file_line { 'IdentityFile'
+file_line { 'IdentityFile':
     path    => '/etc/ssh/ssh_config',
     line    => '    IdentityFile ~/.ssh/school',
-    replace => true,
 }
 
-file_line { 'PasswordAuthenticate'
+file_line { 'PasswordAuthenticate':
     path    => '/etc/ssh/ssh_config',
-    line    => '    PasswordAuthentication no'
-    replace => true,
+    line    => '    PasswordAuthentication no',
 }
