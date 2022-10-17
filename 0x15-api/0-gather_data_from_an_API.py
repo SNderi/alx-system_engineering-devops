16808086690180d79b4c1dd4f051a6db6742ad2c
Sharon Nderi <nderisharonw@gmail.com>
2022-10-17 16:59:02 +0000

    Format output

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -13,12 +13,12 @@ if __name__ == "__main__":
                             .format(sys.argv[1]))
     name = response.json().get("name")
     tasks_total = len(response_todos.json())
-    num_completed = 0
     tasks_completed = []
     for task in response_todos.json():
         if task.get("completed"):
             tasks_completed.append(task.get("title"))
-            num_completed += 1
+
+    num_completed = len(tasks_completed)
     print("Employee {} is done with tasks({}/{}):"
           .format(name, num_completed, tasks_total))
     for title in tasks_completed:
