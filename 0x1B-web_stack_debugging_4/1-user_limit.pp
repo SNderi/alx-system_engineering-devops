495f392ebf8064a4a4c01efe4f8418195161f9cc
Sharon Nderi <nderisharonw@gmail.com>
2022-11-10 21:28:23 +0000

    Add semicolon

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,5 @@
 # Web Stack debugging increase holberton user FD limits
-exec { 'Change-hoberton-user-limits'
+exec { 'Change-hoberton-user-limits':
   environment  => ['DIR=/etc/security/limits.conf',
                    'OHARD=hard nofile 5',
                    'NHARD=hard nofile 5000',
