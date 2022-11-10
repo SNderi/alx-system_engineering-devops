03d9dd4bdc17e34aa1126e1a974118e22c0eb7ef
Sharon Nderi <nderisharonw@gmail.com>
2022-11-10 21:27:22 +0000

    User limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,10 @@
 # Web Stack debugging increase holberton user FD limits
 exec { 'Change-hoberton-user-limits'
-  command  => 'sudo sed -i "s/hard nofile 5/hard nofile 5000" /etc/security/limits.conf; sudo sed -i "s/soft nofile 4/soft nofile 4000" /etc/security/limits.conf',
-  provider => shell
+  environment  => ['DIR=/etc/security/limits.conf',
+                   'OHARD=hard nofile 5',
+                   'NHARD=hard nofile 5000',
+                   'OSOFT=soft nofile 4',
+                   'NSOFT=soft nofile 4000'],
+  command       => 'sudo sed -i "s/$OHARD/$ONEW" $DIR; sudo sed -i "s/$OSOFT/$NSOFT" $DIR',
+  provider      => shell
 }
